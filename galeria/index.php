75784a7325fd7d43e6fb6c867f3ee23555e86735
Joaquín Sanz Bermejo <49841367+joaquinsanz@users.noreply.github.com>
2019-10-23 14:14:16 +0200

    Retoque galeria.php

--- galeria/index.php
+++ galeria/index.php
@@ -1,71 +1,70 @@
 <?php
-
-class App
-{
-
-    public function __construct()
-    {
-        session_start();
-    }
-
-    function subirImagen($archivo, $dir, $conexion){     
-        
-        // Se comprueba que el archivo a subir sea una imagen.
-        if($_FILES[$archivo]["type"] == "image/jpeg"){
-            require(vista.php);    
-            // Se comprueba si ha ocurrido algun error al subir el archivo.
-            if ($_FILES[$archivo]["error"]) {
-                echo '<div class="error">Error '.$_FILES["archivo"]["error"].'al intentar subir el archivo '.$_FILES[$archivo]["name"].'</div>';
-            } else {
-                // Se comprueba si ya se ha creado el subdirectorio para almacenar la imagen.
-                // Y se crea si no existe aun.
-                if(!is_dir("imagenes/".$dir)){
-                    mkdir("imagenes/".$dir, 0755);
-                }
+class App{
     
-                // Comprobamos que no haya ningun archivo con el mismo nombre en el servidor.
-                if (file_exists("imagenes/".$dir."/".$_FILES[$archivo]["name"])) {
-                    echo '<div class="error">Ya hay un archivo con nombre '.$_FILES[$archivo]["name"].'. Renombralo y vuelve a subirlo.</div>';   
-                } else {
-                    // Subimos la imagen.
-                    move_uploaded_file($_FILES[$archivo]["tmp_name"], "imagenes/".$dir."/".$_FILES[$archivo]["name"]);
-                    echo '<div class="subido">Archivo '.$_FILES[$archivo]["name"].' subido.</div>';
-                                            
-                    // Agregamos la imagen a la base de datos.
-                    $consulta = sprintf("INSERT INTO galeriaimagenes (archivo, directorio) VALUES ('%s','%s')",          
-                        $conexion->real_escape_string($_FILES[$archivo]["name"]),
-                        $conexion->real_escape_string($dir)
-                    );
-                        
-                    // Se ejecuta la consulta.
-                    $conexion->query($consulta);
-                }
-            }
-        } else {
-            echo '<div class="error">'.$_FILES[$archivo]["name"].': Formato de archivo no permitido. </div>';
-        }      
+       
+    function subirImagen(){     
         
+        $target_dir = "imagenes/";
+        $target_file = $target_dir . basename($_FILES["imagen1"]["name"]);
+        $uploadIsOk = 1;
+        $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+        // Check if image file is a actual image or fake image
+        if(isset($_POST["submit"])) {
+            $check = getimagesize($_FILES["imagen1"]["tmp_name"]);
+            if($check !== false) {
+                echo "File is an image - " . $check["mime"] . ".";
+                $uploadIsOk = 1;
+            } else {
+                echo "File is not an image.";
+                $uploadOk = 0;
+            }
+        }
+        // Check if file already exists
+        if (file_exists($target_file)) {
+            echo "Sorry, file already exists.";
+            $uploadOk = 0;
+        }
+        // Check file size
+        if ($_FILES["imagen1"]["size"] > 500000) {
+            echo "Sorry, your file is too large.";
+            $uploadOk = 0;
+        }
+        // Allow certain file formats
+        if($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
+        && $imageFileType != "gif" ) {
+            echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            $uploadIsOk = 0;
+        }
+        
+        if ($uploadIsOk == 0) {
+            echo "Sorry, your file was not uploaded.";
+        
+        } else {
+            if (move_uploaded_file($_FILES["imagen1"]["tmp_name"], $target_file)) {
+                header('Location: index.php?method=verGaleria');
+                echo "La imagen ". basename( $_FILES["imagen1"]["name"]). " ha sido subido.";
+            } else {
+                header('Location: index.php?method=verGaleria');
+                echo "Sorry, there was an error uploading your file.";
+            }
+        }
+       
     }
-
-
-$app = new App();
+    public function verGaleria()
+    {
+        $folder_path = 'imagenes/'; 
+        
+        $files = glob($folder_path . "*.{JPG,jpg,gif,png,bmp,jpeg}", GLOB_BRACE);
+        
+        require('vista.php');
+    }
+}
+    
+    
+$App = new App();
 if (isset($_GET['method'])) {
     $method = $_GET['method'];
 } else {
-    $method = 'subirImagen';
+    $method = 'verGaleria';
 }
-if (method_exists($app, $method)) {
-    $app->$method();
-} else {
-    exit('no encontrado');
-}
-
-//esto de aqui cuando puedas lo cambias e intentas hacer los ejercicios de php bien de manera que en vez
-// de sacar un 0 sacaras un 3,5 con lo que suspenderás igual pero no es tan humillante
-// adios
-
-
-
-
-
-?>
+$App->$method();
